36739845bb9df951720778f8f34996f7198f181c
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2024-05-07 20:40:09 -0300

    testing run_consumer

--- subscriber/src/app/messaging_manager.py
+++ subscriber/src/app/messaging_manager.py
@@ -1,41 +1,53 @@
+import json
+import time
 import pika
 import sys
 
 sys.path.append('/home/matheus/Documentos/GIT/FuzzyLogicExp/subscriber/src')
 from shared.shared import (
     RABBITMQ_HOST,
-    EXCHANGE_NAME
+    RABBITMQ_PORT,
+    QUEUE_NAME,
+    EXCHANGE_NAME,
+    PREFETCH_COUNT
 )
 
-class MessagingManager():
-    def __init__(self, *args, **kwargs):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
-        self.channel = self.connection.channel()
-        self.channel.exchange_declare(
-            exchange=EXCHANGE_NAME, 
-            exchange_type='topic'
-        )
+global consuming_time
 
-    def consume(self, keys, callback):
-        result = self.channel.queue_declare('', exclusive=True)
-        queue_name = result.method.queue
-        for key in keys:
-            self.channel.queue_bind(
-                exchange=EXCHANGE_NAME, 
-                queue=queue_name, 
-                routing_key=key)
+# def on_message_received(channel, method, properties, body):
+#     print(f"Received message: {body.decode()}")
+#     if consuming_time == 0:
+#         consuming_time = time.time()
+#     channel.basic_ack(delivery_tag=method.delivery_tag)
+#     if time.time() - consuming_time > 9:
+#         print("Consumer stopped due to timeout.")
+#         channel.stop_consuming()
 
-        self.channel.basic_consume(
-            queue=queue_name, 
-            on_message_callback=callback, 
-            auto_ack=True)
+def start_consuming(queue_name, rabbitmq_host, rabbitmq_port):
+    consuming_time = 0
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port)
+    )
+    channel = connection.channel()
 
-        self.channel.start_consuming()
+    channel.queue_declare(queue=queue_name)
+    channel.basic_qos(prefetch_count=1)
+    #channel.basic_consume(queue=queue_name, on_message_callback=on_message_received)
 
+    print(f"Started consuming messages from queue '{queue_name}'")
+    for method, properties, body in channel.consume(queue=queue_name, inactivity_timeout=1):
+        print(f"Received message: {body.decode()}")
+        channel.basic_ack(method.delivery_tag)
+        if consuming_time == 0:
+            consuming_time = time.time()
+        if time.time() - consuming_time > 9:
+            print("Consumer stopped due to timeout.")
+            channel.cancel()
 
-    def __enter__(self):
-        return self
+if __name__ == "__main__":
+    queue_name = QUEUE_NAME
+    rabbitmq_host = RABBITMQ_HOST
+    rabbitmq_port = RABBITMQ_PORT
 
+    start_consuming(queue_name, rabbitmq_host, rabbitmq_port)
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.conn.close()
--- subscriber/src/app/subscriber.py
+++ subscriber/src/app/subscriber.py
@@ -1,99 +1,39 @@
-import json
+import signal
+import sys
 import threading
 import time
-import sys
-import os
-import logging
 
 sys.path.append('/home/matheus/Documentos/GIT/FuzzyLogicExp/subscriber/src')
 from shared.shared import (
     RABBITMQ_HOST,
     RABBITMQ_PORT,
-    RABBITMQ_USERNAME,
     QUEUE_NAME,
+    EXCHANGE_NAME,
     PREFETCH_COUNT
 )
-from utils.core_functions import save_data_to_csv, calculate_latency
-from messaging_manager import MessagingManager
 
-logging.basicConfig(level=logging.INFO)
-global consumer_thread 
+consumer_thread = None  # Global variable to store the consumer thread
 
-def main():
-    msg_count = [0]
-    latency_data = []
-    is_consuming = True  # Flag to control consumer
-
-    def start_consuming():
-        def callback(ch, method, properties, body):
-            receive_time = time.time()
-            msg_count[0] += 1
-
-            message_data = json.loads(body)
-            publish_time = message_data.get('timestamp')
-
-            latency = calculate_latency(publish_time, receive_time)
-            latency_data.append(latency)
-
-            logging.info(f"""
-                [x] 
-                The publish time is: {publish_time}.
-                Received message with timestamp: {receive_time}.
-                Latency: {latency:.5f} seconds
-            """)
-        with MessagingManager() as consumer:
-            consumer.consume([QUEUE_NAME], callback)
-
-    arrival_rate = 0
-    sample = 0
-    start_counting_time = 0
-    
-    consumer_thread = threading.Thread(target=start_consuming)
+def start_consumer():
+    global consumer_thread
+    consumer_thread = threading.Thread(target=run_consumer)
     consumer_thread.start()
+    print("Consumer started.")
+
+
+def run_consumer():
+    import messaging_manager
+    messaging_manager.start_consuming(queue_name=QUEUE_NAME, rabbitmq_host=RABBITMQ_HOST, rabbitmq_port=RABBITMQ_PORT)
+
+
+
+if __name__ == "__main__":
+    import signal
+
+    start_consumer()
+
+    start_consuming_time = 0
     while True:
-        print("Start consuming")
-        print(f"msg_count: {msg_count[0]}")
-        if start_counting_time == 0:
-            start_counting_time = time.time()
+        time.sleep(1)
+        
 
-        if time.time() - start_counting_time >= 10:
-            print("Stop consuming")
-            is_consuming = False  # Set flag to stop the consumer thread loop
-            consumer_thread.join()  # Wait for the thread to finish consuming messages
-
-            arrival_rate = msg_count[0] / 10
-            average_latency = sum(latency_data) / msg_count[0]
-
-            save_data_to_csv(
-                PREFETCH_COUNT,
-                average_latency,
-                arrival_rate,
-                sample
-            )
-
-            # Reset the counting
-            start_counting_time = 0
-            msg_count[0] = 0
-            latency_data = []
-            arrival_rate = 0
-            sample += 1
-
-            is_consuming = True  # Reset flag for next consumption cycle
-
-            if sample == 30:
-                break
-
-            print("Start consuming again")
-
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            is_consuming = False  # Set flag to stop consumer thread before exiting
-            consumer_thread.join()
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
